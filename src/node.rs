c096df5d00469fe24297293fcc47e3e21fd03fcb
Kevin O'Donnell <kevin@blockchaintp.com>
2019-11-12 13:23:48 -0500

    Always respond to SealRequests if possible
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- src/node.rs
+++ src/node.rs
@@ -125,6 +125,7 @@ impl PbftNode {
             _ => false,
         } && msg_type != PbftMessageType::ViewChange
             && msg_type != PbftMessageType::NewView
+            && msg_type != PbftMessageType::SealRequest
         {
             debug!(
                 "{}: Node is view changing; ignoring {} message",
